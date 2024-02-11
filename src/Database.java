ba1be5166c0b7a4e9f7b7f9ff2b8f73df9715ac8
Caden Swartz <cadenswartz@mines.edu>
2024-02-11 15:29:24 -0700

    initial commit

---
+++ src/Database.java
@@ -0,0 +1,13 @@
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import org.h2.tools.RunScript;
+
+
+public class Database {
+    
+}
