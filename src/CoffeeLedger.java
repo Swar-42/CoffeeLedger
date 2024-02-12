ea7e949cf7a8830f5137840d36bac9598a64e241
Caden Swartz <cadenswartz@mines.edu>
2024-02-11 17:45:15 -0700

    implemented functions for adding to and printing 'orders' and 'people' tables.

--- src/CoffeeLedger.java
+++ src/CoffeeLedger.java
@@ -4,11 +4,22 @@ import java.sql.SQLException;
 public class CoffeeLedger {
 
 
+
+
     public static void main(String[] args) {
+        Database db = new Database();
         try {
-            Database.load();
-        } catch (ClassNotFoundException | SQLException | IOException e) {
-            // TODO Auto-generated catch block
+            // db.addPerson("John");
+            // db.addPerson("Mike");
+            // db.addPerson("Chris");
+
+            db.printPeople();
+            // db.addOrder("Large iced coffee", 4.00);
+            // db.addOrder("small hot coffee", 2.25);
+            db.printOrders();
+
+            db.close();
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
--- src/Database.java
+++ src/Database.java
@@ -10,7 +10,21 @@ import org.h2.tools.RunScript;
 
 
 public class Database {
+
+    private Connection conn;
     
+    public Database() {
+        try {
+            load();
+        } catch (ClassNotFoundException | SQLException | IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void close() throws SQLException {
+        conn.close();
+    }
+
     /**
      * load the database, initializing it if it does not exist, and returning the db connection
      * @return the connection to the database
@@ -18,28 +32,109 @@ public class Database {
      * @throws SQLException cannot get connection with DriverManager or db cannot be initialized
      * @throws IOException db cannot be initialized
      */
-    public static Connection load() throws ClassNotFoundException, SQLException, IOException {
+    public void load() throws ClassNotFoundException, SQLException, IOException {
         Class.forName("org.h2.Driver");
-        Connection conn;
         try {
             conn = DriverManager.getConnection("jdbc:h2:./coffeeledger.db;ifexists=true");
         } catch (SQLException e) {
             // if db does not exist
             conn = DriverManager.getConnection("jdbc:h2:./coffeeledger.db");
-            init(conn);
+            init();
         }
-        return conn;
     }
 
     /**
-     * initializes the database at conn
-     * @param conn - connection from which to initialize the database.
+     * initializes the database
      * @throws SQLException 
      * @throws IOException 
      */
-    public static void init(Connection conn) throws SQLException, IOException {
+    public void init() throws SQLException, IOException {
         // initialize script
-        InputStreamReader isr = new InputStreamReader(new FileInputStream("resources/init.sql"));
+        runScript("resources/init.sql");
+    }
+
+    /**
+     * adds row to the 'people' table
+     * @param name - name of the person to add
+     * @return id of the person added
+     * @throws SQLException 
+     */
+    public void addPerson(String name) throws SQLException {
+        Statement stmt = conn.createStatement();
+        // make sure name does not exist in table
+        String sql = 
+          "SELECT \'" + name + "\' IN "
+        + "  (SELECT name FROM people);"; 
+        ResultSet rs = stmt.executeQuery(sql);
+        rs.next();
+        String s = rs.getString(1);
+        if (s.equals("TRUE")) {
+            throw new IllegalArgumentException(name + " already exists in table people");
+        }
+
+        sql = 
+          "INSERT INTO people (name, bought, paid) "
+        + "VALUES (\'" + name + "\', 0, 0);";
+        stmt.execute(sql);
+    }
+
+    /**
+     * prints the people table
+     * @throws SQLException
+     */
+    public void printPeople() throws SQLException {
+        Statement stmt = conn.createStatement();
+        String query = "SELECT * FROM people";
+        ResultSet rs = stmt.executeQuery(query);
+        while (rs.next()) {
+            System.out.println(rs.getString(1) + " " + rs.getString(2) + " " + rs.getString(3) + " " + rs.getString(4));
+        }
+        rs.close();
+        stmt.close();
+    }
+
+    /**
+     * adds row to the 'orders' table
+     * @param name name of the order
+     * @param price price of the order
+     * @throws SQLException
+     */
+    public void addOrder(String name, double price) throws SQLException {
+        Statement stmt = conn.createStatement();
+        // make sure name does not exist in table
+        String sql = 
+          "SELECT \'" + name + "\' IN "
+        + "  (SELECT name FROM orders);"; 
+        ResultSet rs = stmt.executeQuery(sql);
+        rs.next();
+        String s = rs.getString(1);
+        if (s.equals("TRUE")) {
+            throw new IllegalArgumentException(name + " already exists in table orders");
+        }
+
+        sql = 
+          "INSERT INTO orders (name, price) "
+        + "VALUES (\'" + name + "\', " + price + ");";
+        stmt.execute(sql);
+    }
+
+    /**
+     * prints the orders table
+     * @throws SQLException
+     */
+    public void printOrders() throws SQLException {
+        Statement stmt = conn.createStatement();
+        String query = "SELECT * FROM orders";
+        ResultSet rs = stmt.executeQuery(query);
+        while (rs.next()) {
+            System.out.println(rs.getString(1) + " " + rs.getString(2) + " " + rs.getString(3));
+        }
+        rs.close();
+        stmt.close();
+    }
+
+    private void runScript(String filepath) throws SQLException, IOException {
+        InputStreamReader isr = new InputStreamReader(new FileInputStream(filepath));
         RunScript.execute(conn, isr);
         isr.close();
     }
